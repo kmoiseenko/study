d36423f7cec723ee1b8e5205240d21e1a7a1cb71
Константин Моисеенко <konstantin@MBP-Konstantin.in.ab-soft.net>
2017-11-21 16:02:17 +0200

    Edit structure

--- dev/js/modules/main.jsx
+++ dev/js/modules/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
-import Apod from './components/apod.jsx';
-import Mars from './components/mars.jsx';
+import Apod from '../components/apod.jsx';
+import Mars from '../components/mars.jsx';
 
 class Main extends React.Component {
 	constructor(props) {
--- dev/js/router.jsx
+++ dev/js/router.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
-import App from './app.jsx';
+import App from './modules/app.jsx';
 
 ReactDOM.render((
 	<BrowserRouter>
